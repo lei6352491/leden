08a510ddd34d9363e255b6e6c373ce07118e57e0
root <root@localhost.localdomain>
2019-11-10 08:53:20 +0800

    a

--- caijijava/caiji-core/src/main/java/com/zhouyi/business/core/model/provincecomprehensive/utils/ProvinceZipUtils.java
+++ caijijava/caiji-core/src/main/java/com/zhouyi/business/core/model/provincecomprehensive/utils/ProvinceZipUtils.java
@@ -49,7 +49,7 @@ public class ProvinceZipUtils {
      */
     public static String generatorZip(String classpath,MIS mis,List<DataInfo> dataInfos) throws Exception{
         //生成的名称为： 人员编号.zip
-        classpath+=mis.getPersonInfo().getPersonId();
+        classpath+=mis.getPersonInfo().getPersonId()+"/";
         log.info("文件的存储路径为:"+classpath);
         StringBuffer fileBuffer=new StringBuffer(classpath);
         fileBuffer.append(mis.getPersonInfo().getPersonId());
