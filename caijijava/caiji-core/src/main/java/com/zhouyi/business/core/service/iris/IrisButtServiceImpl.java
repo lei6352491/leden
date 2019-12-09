88cc3a74ce25fc60e8fdf0a8203d0917910a29a6
lei6352491 <root@localhost.localdomain>
2019-12-09 18:57:53 +0800

    a

--- caijijava/caiji-core/src/main/java/com/zhouyi/business/core/service/iris/IrisButtServiceImpl.java
+++ caijijava/caiji-core/src/main/java/com/zhouyi/business/core/service/iris/IrisButtServiceImpl.java
@@ -2,9 +2,11 @@ package com.zhouyi.business.core.service.iris;
 
 
 import com.zhouyi.business.core.dao.LedenCollectIrisMapper;
+import com.zhouyi.business.core.model.xinzhen.IdSecretVersion;
 import com.zhouyi.business.core.model.xinzhen.IrisCommons;
 import com.zhouyi.business.core.model.xinzhen.IrisComparsion;
 import com.zhouyi.business.core.model.xinzhen.IrisReceive;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,11 +20,14 @@ public class IrisButtServiceImpl implements IrisButtService{
 
     @Autowired
     private LedenCollectIrisMapper ledenCollectIrisMapper;
+    @Autowired
+    private IdSecretVersion idSecretVersion;
 
     @Override
     public IrisReceive irisCollect(String rybh) {
 
         IrisReceive irisReceive = ledenCollectIrisMapper.irisCollectSearch(rybh);
+        BeanUtils.copyProperties(idSecretVersion,irisReceive);
         return irisReceive;
     }
 
